739279c546a6452bf3efaed58cc869cc63365cbe
Ronak Malik <ronakmalik4242@gmail.com>
2025-03-01 10:38:40 -0800

    diff data

--- tune.py
+++ tune.py
@@ -9,8 +9,8 @@ def inference():
 
     # Video Inference
     modal = 'video'
-    modal_path = '/volume/VLM_finetune/dataset/Subject 4/Fall/01.mp4'
-    instruct = """What is happening in the video? Does someone fall down? PLEASE RESPOND IN JSON FORMAT
+    modal_path = '/volume/VLM_finetune/dataset/Subject 4/ADL/01.mp4'
+    instruct = """What is happening in the video? Does someone fall down? PLEASE RESPOND IN JSON FORMAT and do not inclde any other text.
     
     Example: Someone falls down
     Output: "{"fall": true}"
